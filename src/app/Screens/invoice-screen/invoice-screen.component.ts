65d0950c3ff4eb1c524d5a5842a5956d86eae291
Davy <davyfungaloi@gmail.com>
2021-05-03 17:08:25 +0200

    added service component for invoices

--- src/app/Screens/invoice-screen/invoice-screen.component.ts
+++ src/app/Screens/invoice-screen/invoice-screen.component.ts
@@ -1,18 +1,29 @@
 import { Component, OnInit } from '@angular/core';
-import {ActivatedRoute} from "@angular/router";
+import { ActivatedRoute } from '@angular/router';
+import invoiceData from '../../../assets/data/invoiceData.json';
+import { Invoice } from '../../models/invoice';
 
 @Component({
   selector: 'app-invoice-screen',
   templateUrl: './invoice-screen.component.html',
-  styleUrls: ['./invoice-screen.component.scss']
+  styleUrls: ['./invoice-screen.component.scss'],
 })
 export class InvoiceScreenComponent implements OnInit {
-  public invoiceId
-  constructor(private route: ActivatedRoute) { }
+  public invoiceId;
+  invoices: Invoice[];
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit(): void {
-    let id = parseInt(this.route.snapshot.paramMap.get('id'))
-    this.invoiceId=id
+    this.invoices = invoiceData;
+    let id = parseInt(this.route.snapshot.paramMap.get('id'));
+    const stringId = id.toString();
+    this.invoiceId = stringId;
+    console.log(id)
+    console.log(stringId);
+    console.log(this.invoices);
+    const currentInvoiceData = this.invoices.filter((invoice) => {
+      return invoice.id == stringId;
+    });
+    console.log('test');
   }
-
 }
